dc131af59fc9e1278b9bc52ed28fd3ddec20eae8
JagatheswariManoharan <jagatheswarimanoharan@Viswanathans-MacBook-Pro.local>
2019-06-26 22:20:44 -0700

    initial commit

---
+++ clicky-game/src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React,{Component}from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+class Navbar extends Component{
+  render(){
+    return (
+      <>
+      <h1>Hello world</h1>
+      </>
+    );
+  }
+}
+ 
+
+
+export default Navbar;
